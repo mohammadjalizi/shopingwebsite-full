712c0b2f6b0cf83efb8c288b36071bb00f24fd6e
mjalizi1376@gmail.com <mjalizi1376@gmail.com>
2024-02-06 14:28:57 +0330

    start project

---
+++ src/pages/Navbar/Navbar.jsx
@@ -0,0 +1,16 @@
+import React from 'react'
+
+const Navbar = () => {
+  return (
+    <div className=' flex justify-between '>
+        
+
+<div>  <h1>icons</h1>   </div>
+<div>  <li> home </li>   </div>
+<div>  shoping icon  </div>
+
+    </div>
+  )
+}
+
+export default Navbar
