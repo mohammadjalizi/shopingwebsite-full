dbff6f618fe62cddae3402e77f2a3ff45c34cbc4
mjalizi1376@gmail.com <mjalizi1376@gmail.com>
2024-02-07 17:02:09 +0330

    install react icon

--- src/pages/Navbar/Navbar.jsx
+++ src/pages/Navbar/Navbar.jsx
@@ -1,13 +1,34 @@
 import React from 'react'
+import { FiShoppingCart } from "react-icons/fi";
+import { FaRegUser } from "react-icons/fa";
 
 const Navbar = () => {
   return (
-    <div className=' flex justify-between '>
+    <div className='   md:mx-96 md:my-5 flex justify-between '>
         
 
 <div>  <h1>icons</h1>   </div>
-<div>  <li> home </li>   </div>
-<div>  shoping icon  </div>
+<div  className=' flex list-none  gap-7 '> 
+    
+     <li> صفحه  اصلی </li> 
+     <li>   فروشگاه </li> 
+     <li>    در بار ه ی ما </li> 
+     
+       </div>
+
+{/* icons */}
+<div className=' flex gap-6 '>  
+    <div>
+
+<FiShoppingCart className='   size-9 '/>
+
+    </div>
+    <div>
+<FaRegUser/>
+
+    </div>
+    
+</div>
 
     </div>
   )
