ef7af6379746ca1952454a2932464198729a198d
mjalizi1376@gmail.com <mjalizi1376@gmail.com>
2024-02-12 13:09:53 +0330

    Add navbar

--- src/pages/Navbar/Navbar.jsx
+++ src/pages/Navbar/Navbar.jsx
@@ -4,15 +4,17 @@ import { FaRegUser } from "react-icons/fa";
 
 const Navbar = () => {
   return (
-    <div className='   md:mx-96 md:my-5 flex justify-between '>
-        
-
-<div>  <h1>icons</h1>   </div>
-<div  className=' flex list-none  gap-7 '> 
+    <div className='    md:my-5 py-4  md:bg-gradient-to-r from-sky-500 to-indigo-500  '>
+  <div className=' md:mx-96 flex justify-between  '>
     
-     <li> صفحه  اصلی </li> 
-     <li>   فروشگاه </li> 
-     <li>    در بار ه ی ما </li> 
+   
+<div className=' md:hidden '>  menu </div>
+<div>  <h1> shoping</h1>   </div>
+<div  className='   hidden md:flex list-none  gap-7 '> 
+    
+     <li className=' cursor-pointer hover:text-pink-400'> صفحه  اصلی </li> 
+     <li className=' cursor-pointer hover:text-pink-400'>   فروشگاه </li> 
+     <li className=' cursor-pointer hover:text-pink-400'>    در بار ه ی ما </li> 
      
        </div>
 
@@ -20,16 +22,16 @@ const Navbar = () => {
 <div className=' flex gap-6 '>  
     <div>
 
-<FiShoppingCart className='   size-9 '/>
+<FiShoppingCart className='   size-7 '/>
 
     </div>
     <div>
-<FaRegUser/>
+<FaRegUser className='  size-7'/>
 
     </div>
     
 </div>
-
+</div>    
     </div>
   )
 }
